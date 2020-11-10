9f0a907aded6e040970c53281e2e93143a85620d
1di4r <01di4r@gmail.com>
2020-11-10 14:24:35 +0300

    Initial Commit

---
+++ Cignifi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cignifi
+//
+//  Created by 1di4r on 11/10/20.
+//  Copyright © 2020 1di4r. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
